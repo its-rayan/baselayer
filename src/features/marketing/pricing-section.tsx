bf9a30221fff02d21d568d7f0c716865a5e60421
its-rayan <rayanmabdirahman@gmail.com>
2026-02-18 20:40:46 +0000

    add header to pricing section

--- src/features/marketing/pricing-section.tsx
+++ src/features/marketing/pricing-section.tsx
@@ -26,43 +26,48 @@ const pricingOptions = [
 export default function PricingSection() {
   return (
     <section id="pricing" className="pt-6">
-      <MarketingContainer className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
-        {pricingOptions.map((option) => (
-          <div className="w-full flex flex-col gap-4 bg-muted-foreground/5 rounded-lg p-6">
-            <div className="flex flex-col gap-2">
-              <h2>{option.name}</h2>
-              <p className="text-muted-foreground">{option.description}</p>
+      <MarketingContainer className="flex flex-col gap-6">
+        <h2>
+          Transparent pricing. No hidden fees. Just great work at a fair rate.
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
+          {pricingOptions.map((option) => (
+            <div className="w-full flex flex-col gap-4 bg-muted-foreground/5 rounded-lg p-6">
+              <div className="flex flex-col gap-2">
+                <h2>{option.name}</h2>
+                <p className="text-muted-foreground">{option.description}</p>
+              </div>
+
+              {option.price ? (
+                <p className="text-lg">
+                  ${option.price} {""}
+                  <em className="text-muted-foreground font-normal text-sm">
+                    /month
+                  </em>
+                </p>
+              ) : (
+                <p className="text-muted-foreground">Contact us</p>
+              )}
+
+              <div className="flex flex-col gap-2">
+                <h3>Includes:</h3>
+                <ul className="list-disc list-inside text-muted-foreground space-y-2">
+                  {option.includes.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+
+              <Button
+                variant={option.price ? "default" : "outline"}
+                size="sm"
+                className="w-full"
+              >
+                {option.price ? "Join today" : "Contact us"}
+              </Button>
             </div>
-
-            {option.price ? (
-              <p className="text-lg">
-                ${option.price} {""}
-                <em className="text-muted-foreground font-normal text-sm">
-                  /month
-                </em>
-              </p>
-            ) : (
-              <p className="text-muted-foreground">Contact us</p>
-            )}
-
-            <div className="flex flex-col gap-2">
-              <h3>Includes:</h3>
-              <ul className="list-disc list-inside text-muted-foreground space-y-2">
-                {option.includes.map((item) => (
-                  <li key={item}>{item}</li>
-                ))}
-              </ul>
-            </div>
-
-            <Button
-              variant={option.price ? "default" : "outline"}
-              size="sm"
-              className="w-full"
-            >
-              {option.price ? "Join today" : "Contact us"}
-            </Button>
-          </div>
-        ))}
+          ))}
+        </div>
       </MarketingContainer>
     </section>
   );
